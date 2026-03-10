05abd048af3b9639484d11d45424470a4cdf5972
Jordan Harband <ljharb@gmail.com>
2026-03-10 12:53:18 -0400

    [Refactor] remove useless ESM entrypoints

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import getPolyfill from 'math.clz32/polyfill';
-
-export default getPolyfill();
-
-export { default as getPolyfill } from 'math.clz32/polyfill';
-export { default as implementation } from 'math.clz32/implementation';
-export { default as shim } from 'math.clz32/shim';
